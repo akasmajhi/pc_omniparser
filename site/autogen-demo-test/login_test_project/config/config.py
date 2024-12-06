0a38778490abf92429e54190f14e05e10b4f349c
Pravash <pravashpanda@gmail.com>
2024-12-06 05:40:14 +0530

    Added Part A in config.py

--- site/autogen-demo-test/login_test_project/config/config.py
+++ site/autogen-demo-test/login_test_project/config/config.py
@@ -1,5 +1,12 @@
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-CHROME_DRIVER_PATH = os.path.join(BASE_DIR, 'resources', 'chromedriver.exe')
-URL = "http://127.0.0.1:5500/site/autogen-demo/index.html"  # Your app URL
+#Part A
+#CHROME_DRIVER_PATH = os.path.join(BASE_DIR, 'resources', 'chromedriver.exe')
+
+#Following is the alternative to ## Part A
+from pathlib import Path
+CHROME_DRIVER_PATH = Path(BASE_DIR) / 'resources' / 'chromedriver.exe'
+
+# Your app URL
+URL = "http://127.0.0.1:5500/site/autogen-demo/index.html"  
