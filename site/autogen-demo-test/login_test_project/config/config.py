9c48a9ae9cd4c63006c2c5c3581908d1f2e84b85
Pravash <pravashpanda@gmail.com>
2024-12-06 07:27:40 +0530

    Part A

--- site/autogen-demo-test/login_test_project/config/config.py
+++ site/autogen-demo-test/login_test_project/config/config.py
@@ -2,13 +2,15 @@ import os
 ################
 # Part A
 ################
-#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-#CHROME_DRIVER_PATH = os.path.join(BASE_DIR, 'resources', 'chromedriver.exe')
-#Following is the alternative to ## Part A
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+CHROME_DRIVER_PATH = os.path.join(BASE_DIR, 'resources', 'chromedriver.exe')
+
+##Following is the alternative to ## Part A
 #from pathlib import Path
 #CHROME_DRIVER_PATH = Path(BASE_DIR) / 'resources' / 'chromedriver.exe'
-# Your app URL
-#URL = "http://127.0.0.1:5500/site/autogen-demo/index.html"  
+
+## Your app URL
+URL = "http://127.0.0.1:5500/site/autogen-demo/index.html"  
 
 ## Added post error for the above 
 ## AttributeError: 'WindowsPath' object has no attribute 'capabilities' 
